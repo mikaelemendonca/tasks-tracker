9712f61b62c3b142c11f75061f20fac07b0ffb45
Mikaele Costa <mikaelemendonca@gmail.com>
2023-03-04 10:35:26 -0300

    Istalando, configurando e criando a primeira rota com vue router

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,7 @@
         </div>
         <div class="column is-three-quarter">
             <div class="box conteudo">
-                <FormTracker @aoSalvarTarefa="salvarTarefa" />
-                <div class="lista">
-                    <TarefaTracker v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index" />
-                </div>
-                <BoxTracker v-if="listaEstaVazia">
-                    Você ainda não inicou tarefas hoje =(
-                </BoxTracker>
+                <router-view></router-view>
             </div>
         </div>
     </main>
@@ -21,34 +15,18 @@
 
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue'
-import FormTracker from './components/FormTracker.vue'
-import TarefaTracker from './components/TarefaTracker.vue'
-import BoxTracker from './components/BoxTracker.vue'
-import ITarefa from './interface/ITarefa';
 
 export default defineComponent({
     name: 'App',
     components: {
-        BarraLateral,
-        FormTracker,
-        TarefaTracker,
-        BoxTracker
+        BarraLateral
     },
     data () {
         return {
-            tarefas: [] as ITarefa[],
             modoEscuroAtivo: false
         }
     },
-    computed: {
-        listaEstaVazia () : boolean {
-            return this.tarefas.length == 0;
-        }
-    },
     methods: {
-        salvarTarefa (tarefa: ITarefa) {
-            this.tarefas.push(tarefa)
-        },
         trocarTema (modoEscuroAtivo: boolean) {
             this.modoEscuroAtivo = modoEscuroAtivo; 
         }
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,44 @@
+<template>
+    <FormTracker @aoSalvarTarefa="salvarTarefa" />
+    
+    <div class="lista">
+        <BoxTracker v-if="listaEstaVazia">
+            Você ainda não inicou tarefas hoje =(
+        </BoxTracker>
+    
+        <TarefaTracker v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index" />
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import FormTracker from '../components/FormTracker.vue'
+import TarefaTracker from '../components/TarefaTracker.vue'
+import BoxTracker from '../components/BoxTracker.vue'
+import ITarefa from '../interface/ITarefa';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        FormTracker,
+        TarefaTracker,
+        BoxTracker
+    },
+    data () {
+        return {
+            tarefas: [] as ITarefa[]
+        }
+    },
+    computed: {
+        listaEstaVazia () : boolean {
+            return this.tarefas.length == 0;
+        }
+    },
+    methods: {
+        salvarTarefa (tarefa: ITarefa) {
+            this.tarefas.push(tarefa)
+        }
+    }
+});
+</script>
