bbc6fbf9d3d7bb0ef17339a145b5ae0f97f3a31d
Mikaele Costa <mikaelemendonca@gmail.com>
2023-03-21 20:39:00 -0300

    Iniciando com Vuex

---
+++ src/store/index.ts
@@ -0,0 +1,25 @@
+
+import IProjeto from "@/interface/IProjeto";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+
+interface Estado {
+    projetos: IProjeto[]
+}
+
+export const key: InjectionKey<Store<Estado>> = Symbol()
+
+export const store = createStore<Estado>({
+    state: {
+        projetos: [
+            {
+                id: new Date().toISOString(),
+                nome: 'Vue3'
+            },
+            {
+                id: new Date().toISOString(),
+                nome: 'PHP'
+            }
+        ]
+    }
+})
