f4bf58adb3e0b40fffa92952e2e341aa091a7ef5
Mikaele Costa <mikaelemendonca@gmail.com>
2023-04-08 17:40:24 -0300

    Separado o cadastro de projetos em uma nova rota para cadastrar e editar

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import FormProjeto from '../views/Projetos/FormProjeto.vue'
 
 const rotas: RouteRecordRaw[] = [
     {
@@ -13,7 +14,17 @@ const rotas: RouteRecordRaw[] = [
         path: '/projetos',
         name: 'Projetos',
         component: Projetos
-    }
+    },
+    {
+        path: '/projetos/novo',
+        name: 'Novo projetos',
+        component: FormProjeto
+    },
+    {
+        path: '/projetos/:id',
+        name: 'Editar projetos',
+        component: FormProjeto
+    },
 ]
 
 const roteador = createRouter({
